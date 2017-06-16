6b74232cc7b7365d8eb6369a80bb5bbd3470aa8a
Alex Testin <alextestin@Alexs-MacBook-Pro.local>
2017-06-16 15:02:40 -0500

    Created project in Xcode and added it to repository

---
+++ SwiftLanguageBasics/SwiftLanguageBasics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftLanguageBasics
+//
+//  Created by Alex Testin on 6/16/17.
+//  Copyright © 2017 Alex Testin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
